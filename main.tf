c5fbef8c723fe59a54a746e803d779dc3072c1f7
Chibuzor Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2026-02-10 11:43:23 +0200

    added infrastructure

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = ">=1.9.0"
+  cloud {
+    organization = "victorblaze22"
+    workspaces {
+      name = "azure-terraform-cicd"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.8.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  #subscription_id = "c0239a41-ee80-487d-bb52-bc38e45b9cb5"
+}
+
+resource "azurerm_resource_group" "dev_rg" {
+  location = "West Europe"
+  name     = "devops"
+}
+
+resource "azurerm_storage_account" "dev_account" {
+  name                     = "stgvic2012DEV"
+  resource_group_name      = azurerm_resource_group.dev_rg.name
+  location                 = azurerm_resource_group.dev_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "development"
+  }
+}
